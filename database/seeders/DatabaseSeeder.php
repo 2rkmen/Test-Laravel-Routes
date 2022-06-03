6354bfefce9dc503ea8365f94569d5dbea348c15
akurbanov <maotsedun@mail.ru>
2022-06-03 14:17:11 +0300

    27 laracast DatabaseSeeder.php - шикарная тема, чтобы заполнять базу тестовыми данными. Сильно сэкономит время
    для обновления структуры бд:
    дропаются все таблицы, создаются заново из миграций, --seed - заполняются тестовыми данными
    php artisan migrate:fresh --seed
    ----
    а если просто заполнить: php artisan db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+        $hobbies = Category::create([
+            'name' => 'Hobbies',
+            'slug' => 'hobbies'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => "my family post",
+            'slug' => "my-family-post",
+            'excerpt' => "excerpt family post",
+            'body' => "family lorem ipsum dolar sit ames",
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => "my work post",
+            'slug' => "my-work-post",
+            'excerpt' => "excerpt work post",
+            'body' => "work lorem ipsum dolar sit ames",
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $hobbies->id,
+            'title' => "my hobby post",
+            'slug' => "my-hobby-post",
+            'excerpt' => "excerpt hobby post",
+            'body' => "hobby lorem ipsum dolar sit ames",
+        ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
